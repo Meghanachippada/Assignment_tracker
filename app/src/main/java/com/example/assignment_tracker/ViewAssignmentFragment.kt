e4008a6806c591458ab227a72575eca1505b638f
rhanakeawe <rhanakeawe@gmail.com>
2024-12-12 19:28:55 -0800

    delete button on Assignment works (but you have to reload page to see)

--- app/src/main/java/com/example/assignment_tracker/ViewAssignmentFragment.kt
+++ app/src/main/java/com/example/assignment_tracker/ViewAssignmentFragment.kt
@@ -1,6 +1,7 @@
 package com.example.assignment_tracker
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -10,6 +11,9 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.auth.FirebaseAuth
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class ViewAssignmentFragment : Fragment() {
 
@@ -43,9 +47,48 @@ class ViewAssignmentFragment : Fragment() {
         viewModel.assignments.observe(viewLifecycleOwner) { assignments ->
             recyclerView.adapter = AssignmentAdapter(assignments, object : AssignmentAdapter.OnAssignmentDeleteListener {
                 override fun onAssignmentDelete(assignment: Assignment, position: Int) {
+                    findAssignment(assignment)
                     Toast.makeText(requireContext(), "${assignment.assignmentName} deleted", Toast.LENGTH_SHORT).show()
                 }
             })
         }
+    }
+
+    private fun findAssignment(inputAssignment: Assignment) {
+        println(RetrofitInstance.api.getAssignmentsByUserEmail("\"assignmentName\"","\"${inputAssignment.assignmentName}\"").request().url())
+        RetrofitInstance.api.getAssignmentsByUserEmail("\"assignmentName\"","\"${inputAssignment.assignmentName}\"").enqueue(object :
+            Callback<Map<String, Assignment>> {
+            override fun onResponse(
+                call: Call<Map<String, Assignment>>,
+                response: Response<Map<String, Assignment>>
+            ) {
+                if (response.isSuccessful) {
+                    val assignments = response.body()
+                    if (assignments != null) {
+                        for ((id, assignment) in assignments) {
+                            deleteAssignment(id)
+                        }
+                    }
+                }
+            }
+
+            override fun onFailure(call: Call<Map<String, Assignment>>, t: Throwable) {
+                Log.e("ViewAssignment","Failed to find assignment to delete")
+            }
+        })
+    }
+
+    private fun deleteAssignment(id: String) {
+        RetrofitInstance.api.deleteAssignment(id).enqueue(object : Callback<Void> {
+            override fun onResponse(call: Call<Void>, response: Response<Void>) {
+                if (response.isSuccessful) {
+                    Log.i("ViewAssignment","Deleted assignment $id")
+                }
+            }
+
+            override fun onFailure(call: Call<Void>, t: Throwable) {
+                Log.e("ViewAssignment","Failed to delete assignment")
+            }
+        })
     }
 }
